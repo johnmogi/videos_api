6884d31d13812c6fcde94935a36e8e84eaecac79
john mogi <anguru@gmail.com>
2019-08-16 11:43:19 +0300

    no-change

--- bikesapi/bikesModel.js
+++ bikesapi/bikesModel.js
@@ -1,7 +1,35 @@
 $( document ).ready(function() {
 
+
+  
+
     const callforbike = $("#callforbike")
     const companyChoosecallforbike = $("#companyChoose")
     const callserchdbforbike = $("#serchdb")
 
+
+//     bcCompanies.map((str) => {
+//         const {length} = str;
+//         return length;
+//       });
+
+// // try to build array of cities in select
+
+
+// // const citiesMapped = bcCompanies.filter(({ city }) => city > 18 );
+
+// const citiesMapped = bcCompanies.reduce((acc, it) => {
+//     acc[company.city] = acc[company.city] || [];
+//     return acc;
+//   }, {});
+
+// console.log(acc)
 });
+
+// var cities = bcCompanies.networks.city
+
+// const cityesMapped = networks.map((city2) => {
+//         const {city} = city2;
+//         return city2;
+//       });
+
