31f1ea59bcf75185111c30094db82d05369114fb
johnmogi <anguru@gmail.com>
2019-08-25 00:20:08 -0700

    new

--- bikesapi/bikesModel.js
+++ bikesapi/bikesModel.js
@@ -1,29 +1,23 @@
 
-var networks = bcCompanies.networks
-var location = bcCompanies.location
-
-let findBike2 = callforbike.value
-   
-// function searchdb(){
-//     const citymap = bcCompanies.filter(companypilot => company.city === "cities");
-
-// }
-
-$( document ).ready(function() {
-
-    const callforbike = $("#callforbike")
-    const companyChoosecallforbike = $("#companyChoose")
-    const callserchdbforbike = $("#serchdb")
-
-  //     $( "#serchdb").on("click", smaller)
-           
-  //  function smaller(){ 
   
-  //   const allcity = location.map(city => {
-  //     return location.city;
-  //   });
-  //   console.log(allcity);
-  //   }
-      // let officersIds = officers.map(officer => officer.id);
+    
+$(function() {
+ 
 
-});
+    }); // RF
+
+
+  let networks = bcCompanies.networks
+  const citiesForCompanies = bcCompanies.networks.reduce((companiesObj, Bccompany) => {
+    let companyName = Bccompany.company
+   
+    let companies = Bccompany.location
+    let city = companies.city
+    let beforeObj = companiesObj[city] || []
+    // console.log(city)
+
+   $("#callforbike").text("option").text(city)
+
+    return {...companiesObj,[city]: [...beforeObj,companyName]}
+    }, {})
+
